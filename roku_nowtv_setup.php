b147744e66fb3aad5cc627fb04f616a792fa240c
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-05 11:00:48 +0000

    no message

--- roku_nowtv_setup.php
+++ roku_nowtv_setup.php
@@ -1,13 +1,46 @@
 <?php
 
-function keypress($ip_address, $key){
-	execl();
+# Name: Roku NowTV Setup Script
+# Author: GTV
+# Created: 04 Feb 2019
+# Version 1.0
+
+
+// vars
+$ip_address = $argv[1];
+
+
+// functions
+function keypress($ip_address, $key, $sleep = '1'){
+	exec("curl --silent --output /dev/null -d '' http://".$ip_address.":8060/keypress/".$key);
+	sleep($sleep);
 }
 
-function load_channel($ip_address, $channel_id, $content_id = ''){
+function load_channel($ip_address, $channel_id, $content_id = '', $sleep = '1'){
 	exec("curl --silent --output /dev/null -d '' http://".$ip_address.":8060/launch/".$channel_id."?contentID=".$content_id);
+	sleep($sleep);
 }
 
-// curl -d '' http://$IP_ADDRESS:8060/launch/20242?contentID=1402
 
-load_channel('192.168.1.180', '20242', 1402);
+// load NowTV ready for initial login
+load_channel($ip_address, '20242', 1402);
+
+// enter username text box
+keypress($ip_address, 'Select', 1);
+
+keypress($ip_address, 'Down', 1);
+keypress($ip_address, 'Down', 1);
+keypress($ip_address, 'Right', 1);
+keypress($ip_address, 'Right', 1);
+keypress($ip_address, 'Right', 1);
+keypress($ip_address, 'Right', 1);
+keypress($ip_address, 'Right', 1);
+keypress($ip_address, 'Right', 1);
+keypress($ip_address, 'Select', 1);
+
+keypress($ip_address, 'Back', 1);
+keypress($ip_address, 'Select', 1);
+
+keypress($ip_address, 'Down', 1);
+keypress($ip_address, 'Down', 1);
+keypress($ip_address, 'Select', 1);
