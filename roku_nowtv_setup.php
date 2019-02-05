936d0e00e504a358292f7697962ca78c9788b426
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-05 10:35:10 +0000

    no message

--- roku_nowtv_setup.php
+++ roku_nowtv_setup.php
@@ -8,3 +8,6 @@ function load_channel($ip_address, $channel_id, $content_id = ''){
 	exec("curl -d '' http://".$ip_address.":8060/launch/".$channel_id."?contentID=".$content_id);
 }
 
+// curl -d '' http://$IP_ADDRESS:8060/launch/20242?contentID=1402
+
+load_channel('192.168.1.180', '20242', 1402);
---
+++ update.sh
@@ -0,0 +1,1 @@
+cd /home/whittinghamj/iptv_scripts && git --git-dir=/home/whittinghamj/iptv_scripts/.git pull origin master
