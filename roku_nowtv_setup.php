4a9394027a283ba959484f160059a0b3d36c06a9
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-05 11:28:35 +0000

    no message

--- roku_nowtv_setup.php
+++ roku_nowtv_setup.php
@@ -11,6 +11,83 @@ $ip_address = $argv[1];
 
 
 // functions
+nowtv_login_keyboard($ip_address, $key){
+	if($key == '1'){
+		
+	}
+	if($key == '2'){
+		keypress($ip_address, 'Right', '0');
+	}
+	if($key == '3'){
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+	}
+	if($key == '4'){
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+	}
+	if($key == '5'){
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+	}
+	if($key == '6'){
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+	}
+	if($key == '7'){
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+	}
+	if($key == '8'){
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+	}
+	if($key == '9'){
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+	}
+	if($key == '0'){
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+		keypress($ip_address, 'Right', '0');
+	}
+
+	// select item
+	keypress($ip_address, 'Select', '1');
+
+	// reset keyboard for next key
+	keypress($ip_address, 'Back', '2');
+	keypress($ip_address, 'Select', '1');
+}
+
+
 function keypress($ip_address, $key, $sleep = '1'){
 	exec("curl --silent --output /dev/null -d '' http://".$ip_address.":8060/keypress/".$key);
 	sleep($sleep);
@@ -30,65 +107,15 @@ echo "Loading NowTV login screen. \n";
 keypress($ip_address, 'Select', 2);
 
 // letter j
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Select', 0);
-
-// reset for next letter
-keypress($ip_address, 'Back', 2);
-keypress($ip_address, 'Select', 1);
-
-// letter a
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Select', 1);
-
-// reset for next letter
-keypress($ip_address, 'Back', 2);
-keypress($ip_address, 'Select', 1);
-
-// letter m
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Select', 0);
-
-// reset for next letter
-keypress($ip_address, 'Back', 2);
-keypress($ip_address, 'Select', 1);
-
-// letter i
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Select', 0);
-
-// reset for next letter
-keypress($ip_address, 'Back', 2);
-keypress($ip_address, 'Select', 1);
-
-// letter e
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Right', 0);
-keypress($ip_address, 'Down', 0);
-keypress($ip_address, 'Select', 1);
+nowtv_login_keyboard($ip_address, '1');
+nowtv_login_keyboard($ip_address, '2');
+nowtv_login_keyboard($ip_address, '3');
+nowtv_login_keyboard($ip_address, '4');
+nowtv_login_keyboard($ip_address, '5');
+nowtv_login_keyboard($ip_address, '6');
+nowtv_login_keyboard($ip_address, '7');
+nowtv_login_keyboard($ip_address, '8');
+nowtv_login_keyboard($ip_address, '9');
+nowtv_login_keyboard($ip_address, '0');
 
 
